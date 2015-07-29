8324a982eda066cccb36fc02e6a4866428e73dd5
Renee <rlwise@oakland.edu>
2015-07-29 11:46:07 -0400

    method skeletons more documentation and more added later

--- source/edu/oakland/classProject/production/Core.java
+++ source/edu/oakland/classProject/production/Core.java
@@ -2,6 +2,7 @@ package edu.oakland.classproject.production
 
 /**
 *This class holds all of the core functionality of TheGuessingGame backend.
+* Calculations for the application are performed in this class.
 *@author
 *@version "version 0.0" "150729"
 */
@@ -12,4 +13,40 @@ public class Core {
 	/**
 	*TODO: make sure to define each method's functionality
 	*/
+	
+	/**startGame() {
+	
+	}
+	
+	endGame() {
+	
+	} 
+	
+	void setUpperBoundInput() {
+	
+	}
+	
+	 int getUpperRange() {
+	
+		return upperRange; //1024 or 1023?
+	
+	}
+	
+	void receiveChoice() {
+	
+	}
+	
+	void computeSystemGuess() {
+	
+	}
+	
+	int getMaxNumGuesses() {
+		return maxNumGuesses; // 10 for non extra credit, log base 2 result of upper bound for extra credit
+	}
+
+	iterateCurrentGuess() {
+	
+	} */
+	
+	
 }
