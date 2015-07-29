b4a2d40a363d686e9bb01837c82fcf72360f6bd5
Greg Kabacinski <gdkabacinski@oakland.edu>
2015-07-29 10:41:39 -0400

    Updated team2 requirements, added templates to java class files.

---
+++ source/edu/oakland/classProject/production/Core.java
@@ -0,0 +1,15 @@
+package edu.oakland.classproject.production
+
+/**
+*This class holds all of the core functionality of TheGuessingGame backend.
+*@author
+*@version "version 0.0" "150729"
+*/
+public class Core {
+	/**
+	*TODO: class variable definitions here
+	*/
+	/**
+	*TODO: make sure to define each method's functionality
+	*/
+}
---
+++ source/edu/oakland/classProject/production/Display.java
@@ -0,0 +1,16 @@
+package edu.oakland.classproject.production
+
+/**
+*This class holds the methods involved with displaying the command line interface.
+*@author
+*@version "version 0.0" "150729"
+*/
+public class Display {
+	/**
+	*TODO: class variable definitions here
+	*/
+	/**
+	*TODO: make sure to define each method's functionality
+	*/
+}
+
