e2524c8a63ba13fda6698941cdb5ffcac8f385a6
btokarz <barttokarz@gmail.com>
2015-08-05 11:37:38 -0400

    Updated scanners

--- source/edu/oakland/classProject/production/Display.java
+++ source/edu/oakland/classProject/production/Display.java
@@ -12,15 +12,16 @@ public class Display {
 	/**
 	*TODO: class variable definitions here
 	*/
-	Scanner input = new Scanner(System.in);
 	/**
 	*TODO: make sure to define each method's functionality
 	*/
 	public char displayWelcomeMessage() {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Welcome to The Guessing Game!");
 		System.out.println("-----------------------------");
 		System.out.println("Select an option:");
-		System.out.println("[P]: Play");
+		System.out.println("[S]: Simple play");
+		System.out.println("[A]: Advanced play");
 		System.out.println("[Q]: Quit");
 		System.out.println();
 		System.out.print(">");
@@ -29,6 +30,7 @@ public class Display {
 	}
 	
 	public int displayRequestUpperBound(){
+		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a value for a custom upper bound or press [ENTER]");
 		System.out.println("to accept the default value of 1024.");
 		System.out.println("The value must be a positive whole number greater than 1.");
@@ -36,7 +38,8 @@ public class Display {
 		return userUpperBound;
 	}
 	
-	public void displayBeginScreen (int calculatedUpperRange, int maxNumGuesses) {
+	public char displayBeginScreen (int calculatedUpperRange, int maxNumGuesses) {
+		Scanner input = new Scanner(System.in);
 		System.out.println("Please think of a number between 1 and " + calculatedUpperRange.toString() + ".");
 		System.out.println();
 		System.out.println("Make sure you remember what your number is");
@@ -46,9 +49,11 @@ public class Display {
 		System.out.println();
 		System.out.println("Press any key when you are ready to begin the game!");
 		char beginGame = input.nextChar();
+		return beginGame;
 	}
 	
 	public char displayGameScreen () {
+		Scanner input = new Scanner(System.in);
 		System.out.println("");
 	}
 	
