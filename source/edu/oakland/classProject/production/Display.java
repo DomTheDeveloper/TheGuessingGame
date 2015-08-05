9fa16bec61121718ffd16477b678d8648724c2d1
btokarz <barttokarz@gmail.com>
2015-08-05 11:26:56 -0400

    Defined upper bound method

--- source/edu/oakland/classProject/production/Display.java
+++ source/edu/oakland/classProject/production/Display.java
@@ -1,4 +1,4 @@
-package edu.oakland.classproject.production
+package edu.oakland.classproject.production;
 
 import java.util.*;
 import java.io.*;
@@ -13,9 +13,6 @@ public class Display {
 	*TODO: class variable definitions here
 	*/
 	Scanner input = new Scanner(System.in);
-	char playOrQuit;
-	char beginGame;
-	int userUpperBound;
 	/**
 	*TODO: make sure to define each method's functionality
 	*/
@@ -27,16 +24,19 @@ public class Display {
 		System.out.println("[Q]: Quit");
 		System.out.println();
 		System.out.print(">");
-		playOrQuit = input.nextChar();
-		return playOrQuit;
+		char playOrQuit = input.nextChar();
+		return playOrQuit.toUpperCase();
 	}
 	
 	public int displayRequestUpperBound(){
-		System.out.println("");
-		System.out.println("");
+		System.out.println("Enter a value for a custom upper bound or press [ENTER]");
+		System.out.println("to accept the default value of 1024.");
+		System.out.println("The value must be a positive whole number greater than 1.");
+		int userUpperBound = input.nextInt();
+		return userUpperBound;
 	}
 	
-	public char displayBeginScreen (int calculatedUpperRange, int maxNumGuesses) {
+	public void displayBeginScreen (int calculatedUpperRange, int maxNumGuesses) {
 		System.out.println("Please think of a number between 1 and " + calculatedUpperRange.toString() + ".");
 		System.out.println();
 		System.out.println("Make sure you remember what your number is");
@@ -44,8 +44,12 @@ public class Display {
 		System.out.println();
 		System.out.println("The game will guess what your number is within " + maxNumGuesses.toString() + "guesses or less.");
 		System.out.println();
-		System.out.println("Press [P] when you are ready to begin the game!");
-		beginGame = input.nextChar();
-		return beginGame;
+		System.out.println("Press any key when you are ready to begin the game!");
+		char beginGame = input.nextChar();
 	}
+	
+	public char displayGameScreen () {
+		System.out.println("");
+	}
+	
 }
