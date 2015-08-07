64b3d6f05803fdefe7648918b05b37d9d921eb65
DomTheDeveloper <dominic.dabish@gmail.com>
2015-08-07 01:08:59 -0400

    deleted unneeded batch file

--- RunClassProjectProduction.bat
+++
@@ -1,8 +0,0 @@
-@SET /P _class=Enter the production class:  edu.oakland.classProject.production.
-@SET _path=%_class:.=\%
-
-javac -d bin -sourcepath source source\edu\oakland\classProject\production\%_path%.java
-
-java -cp bin edu.oakland.classProject.production.%_class%
-
-@pause
