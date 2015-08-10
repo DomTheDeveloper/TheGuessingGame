d8f157359530b006a9fcd99d02d4df583164db68
btokarz <barttokarz@gmail.com>
2015-08-10 10:40:22 -0400

    added main comments

--- source/edu/oakland/classProject/production/Main.java
+++ source/edu/oakland/classProject/production/Main.java
@@ -3,16 +3,29 @@ package edu.oakland.classProject.production;
 import edu.oakland.classProject.production.Display;
 import edu.oakland.classProject.production.Core;
 
-public class Main{
+/**
+*	This class passes information between the Core and the Display class.
+*	@author Gregory Kabacinski
+*	@version "version 0.2" "150810"
+**/
 
+public class Main{
+	
+	// Create instances of the Display and Core classes.
 	private static Display display = new Display();
 	private static Core core = new Core();
 	
+	// The main method calls for the methods to start the game and then method for
+	// the system to guess and the player to interact with the system.
 	public static void main(String[] args){
 		startGame();
 		makeGuess("higher");
 	}
-	
+
+	/** 
+	*	The startGame method calls for the Display class to display the initial screen.
+	*	This will also read the initial user input for simple/advanced game selection.
+	**/
 	public static void startGame(){
 		char playSelection = display.getPlaySelection();
 		switch (playSelection){
