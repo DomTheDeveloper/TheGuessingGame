d1a37c6a2fe5e1e7b659830376e61f1f63a5408b
Renee <rlwise@oakland.edu>
2015-08-09 22:38:01 -0400

    changed to reflect SD and pass tests
    This version is NOT final. JavaDoc. I will be collaborating with David
    to see if any changes are needed and same with the version he made.
    Also, I David and I will be collaborating with the test team to make
    sure the necessary parts are tested and all the tests pass. This should
    be finalized by Monday night 8/10 with the Javadoc added too.

--- source/edu/oakland/classProject/production/Core.java
+++ source/edu/oakland/classProject/production/Core.java
@@ -1,4 +1,4 @@
-package edu.oakland.classProject.production
+package edu.oakland.classProject.production; // classProject be lowercase?
 
 /**
 * This class holds all of the core functionality of TheGuessingGame backend.
@@ -17,63 +17,38 @@ package edu.oakland.classProject.production
 
 // upperbound 
 public class Core {
-	/**
-	*TODO: class variable definitions here
-	*/  
-	private static inputUpperRange = 1024;	// The maximum number the user can pick //made final in mian
-	private static calcUpperRange;		// Adjusts the upper range so the user can pick
-						// a number from 0-1023.
-	private static maxNumGuesses; 		// The number of guesses the System is
-						// allotted. If the System does not guess
-						// the number correctly in the guesses
-						// allotted to it, the System loses and
-						// the user wins.
+
+	private static boolean hasGameEnded = false;// Flag for if a condition was encountered
+											// that signifies the end of the game.
+	
+	private static int inputUpperBound = 1024;	// The maximum number the user can pick.
+											// 1024 by default. The user may also specify
+											// this value.
+	
+	private static int calcUpperBound;		// Adjusts the upper bound so the user can pick
+											// a number from 1-1023.
+	private static int maxNumGuesses; 		// The number of guesses the System is
+											// allotted. If the System does not guess
+											// the number correctly in the guesses
+											// allotted to it, the System loses and
+											// the user wins.
 						
-	private static currentGuess;
-	private static previousGuess = 0;
-	private static nthGuess = 1;
+	private static int numberGuessed = 0; 	// The number the game generates as a guess for each pass 
+											// until the number is guessed, the last  iteration is the 
+											// correct guess
+	private static int previousGuess = 0;
+	private static int currentGuess = 0;	// Counter for the current guess count of the program that 
+											// updates with each iteration of the guess cycle		
 	
-	/**
-	* Returns the maximum number the in the range of numbers that the
-	* user can pick.
-	*@return returns the value of "inputUpperRange" of type "int".
-	*/
-	 public int getInputUpperRange() {
-		return inputUpperRange;
+	private static String feedbackSelection = ""; 	
+											// The selection that the user specifies in response to the 
+											// System's guess. The user may specify that the number
+											// guessed is higher, lower, or equal to the number he/she 
+											// picked.
+	
+	private void computeMaxNumGuesses() {
+	    maxNumGuesses = (int)(Math.log(inputUpperBound)/Math.log(2));
 	}
-	
-	/**
-	* Returns the maximum number the in the range of numbers that the
-	* user can pick.
-	*@return returns the value of "inputUpperRange" of type "int".
-	*/
-	public void calcUpperRange() {
-		calcUpperRange = inputUpperRange - 1;	
-	}
-	
-	/**
-	* For extra credit for when the user can set the upper range.
-	*/
-	public void setInputUpperRange() {
-	}
-	
-	public void setNumGuesses() {
-	    maxNumGuesses = (int)(Math.log(inputUpperRange)/Math.log(2));
-	}
-	
-	public int getNumGuesses() {
-	    return maxNumGuesses;
-	}
-	
-	/**
-	* 
-	*/
-	public void computeLowerGuess() {
-	    currentGuess = Math.ceil(previousGuess - (inputUpperBound/nthGuess));
-	}
-	
-	public void computeHigherGuess() {
-	    currentGuess = Math.ceil(previousGuess + (inputUpperBound/nthGuess));
 	
 	/**
 	* Returns the maximum number of guesses the System is 
@@ -81,16 +56,83 @@ public class Core {
 	* the user wins the game and the System loses.
 	*@return returns the value of "maxNumGuesses" of type "int".
 	*/
-	public int getMaxNumGuesses() {
-		return maxNumGuesses;
+	public int requestMaxNumGuesses() {
+		computeMaxNumGuesses();
+	    return maxNumGuesses;
+	}
+	
+	/**
+	* 
+	*/
+	private void computeGuess() {
+		
+		if(feedbackSelection.equalsIgnoreCase("lower")) {
+			numberGuessed = (int) Math.ceil(previousGuess - 
+							(inputUpperBound/currentGuess));
+		}
+		else if(feedbackSelection.equalsIgnoreCase("higher")) {
+			numberGuessed = (int) Math.ceil(previousGuess + 
+							(inputUpperBound/currentGuess));
+		}
+		previousGuess = numberGuessed;
+	}
+	
+	public int getGuess() {
+		computeGuess();
+		return numberGuessed;
 	}
 
 	/**
 	* Iterates the guess counter when the System guesses a number.
 	*/
-	private void iterateCurrentGuess() {
-		++nthGuess;
+	private void computeGuessIteration() {
+		++currentGuess;
 	}
-
+	
+	/**
+	*
+	*/
+	public int getGuessIteration() {
+		computeGuessIteration();
+		return currentGuess;
+	}
+	
+	public void setGuessFeedbackSelection(String selection) {
+		feedbackSelection = selection;
+	}
+		
+	private void computeHasGameEnded() {
+		if(currentGuess >= 10)
+			hasGameEnded = true;
+		else if(feedbackSelection.equalsIgnoreCase("equal"))
+			hasGameEnded = true;
+		else
+			hasGameEnded = false;
+	}
+	
+	public boolean requestHasGameEnded() {
+		computeHasGameEnded();
+		return hasGameEnded;
+	}
+	
+	/**
+	* For extra credit for when the user can set the upper bound.
+	*/
+	public void setUpperBoundInput(int chosenUpperBound) {
+		inputUpperBound = chosenUpperBound;
+	}
+	
+	/**
+	* Adjusts the upper bound to the specifed upper bound - 1.
+	*@return returns the value of "calcUpperBound" of type "int".
+	*/
+	private void computeUpperBoundComputed() {
+		calcUpperBound = inputUpperBound - 1;	
+	}
+	
+	public int requestUpperBoundComputed() {
+		computeUpperBoundComputed();
+		return calcUpperBound;
+	}
 }
 
