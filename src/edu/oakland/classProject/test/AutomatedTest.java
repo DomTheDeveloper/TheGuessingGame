d97025c96b842921f3f8aa69450b0cf3846004a3
Greg Kabacinski <gdkabacinski@oakland.edu>
2015-08-16 16:57:14 -0400

    Added brute force test to AutomatedTest

--- src/edu/oakland/classProject/test/AutomatedTest.java
+++ src/edu/oakland/classProject/test/AutomatedTest.java
@@ -12,11 +12,14 @@ import edu.oakland.classProject.production.Main;
 */
 
 public class AutomatedTest extends TestCase{
-
-	public void testGame(){
+	
+	/**
+	* Tests the game functionality with one specific userNumber and upperBound
+	*/
+	public void testGameSingle(){
 		int userNumber = 1000;
-		int upperBound = 10;
-		AutomatedDisplay display = new AutomatedDisplay(userNumber, upperBound);
+		int upperBoundInput = 10;
+		AutomatedDisplay display = new AutomatedDisplay(userNumber, upperBoundInput);
 		Main main = new Main(display);
 		
 		while(main.startGame()){
@@ -26,5 +29,47 @@ public class AutomatedTest extends TestCase{
 			main.endGame();
 		}
 		assertEquals(display.getEndGuess(), userNumber);
+	}
+
+	/**
+	* Tests the game functionality for every userNumber within a specified upperBound
+	*/
+	public void testGameAll(){
+		int userNumber = 1;
+		int upperBoundInput = 16;
+		int upperBound = 65536;
+		AutomatedDisplay display;
+		Main main;
+		while(userNumber < upperBound) {
+			display = new AutomatedDisplay(userNumber, upperBoundInput);
+			main = new Main(display);
+			
+			while(main.startGame()){
+				while(main.makeGuess()){
+					main.giveFeedback();
+				}
+				main.endGame();
+			}
+		assertEquals(display.getEndGuess(), userNumber);
+		userNumber++;
+		}
+	}
+
+	/**
+	* Tests the game for a userNumber that is outside the upper bound
+	*/
+	public void testGameFail(){
+		int userNumber = 1050;
+		int upperBoundInput = 10;
+		AutomatedDisplay display = new AutomatedDisplay(userNumber, upperBoundInput);
+		Main main = new Main(display);
+		
+		while(main.startGame()){
+			while(main.makeGuess()){
+				main.giveFeedback();
+			}
+			main.endGame();
+		}
+		assertFalse(userNumber == display.getEndGuess());
 	}
 }
