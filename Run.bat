7dda1b58d8d81bbf58f382aa64fd906c463ffe23
Greg Kabacinski <gdkabacinski@oakland.edu>
2015-08-16 18:22:47 -0400

    Added gh-pages branch

--- Run.bat
+++
@@ -1,4 +0,0 @@
-@javac -d bin -sourcepath src src\edu\oakland\classProject\cmdLine\RunProgram.java
-@java -cp bin edu.oakland.classProject.cmdLine.RunProgram
-
-@pause
--- generateDocs.bat
+++
@@ -1,1 +0,0 @@
-@javadoc -d javadocs -cp c:\junit3.8.2\junit.jar -sourcepath src edu.oakland.classProject.production edu.oakland.classProject.test 2> stderr.txt
